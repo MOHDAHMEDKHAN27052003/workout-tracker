176e2c6eef9d896a65b6d64fa99849159f419a15
MOHDAHMEDKHAN27052003 <mohd.ahmedkhan190@gmail.com>
2025-06-03 16:57:58 +0530

    nav fixed

--- src/components/HomeNav.jsx
+++ src/components/HomeNav.jsx
@@ -3,9 +3,10 @@ import { NavLink } from "react-router-dom";
 function HomeNav() {
     return (
         <>
-            <div className="flex justify-evenly m-4 p-4 sm:mx-28 md:mx-40 lg:mx-100 rounded-2xl bg-gray-500">
-                <NavLink to="/" className={e => e.isActive ? "text-blue-500" : 'text-white'}>Home</NavLink>
-                <NavLink to="/workouts" className={e=>e.isActive?"text-blue-500":"text-white"}>Workouts</NavLink>
+            <div className="flex justify-evenly m-4 p-4 sm:mx-28 md:mx-60 lg:mx-120 rounded-2xl bg-gray-500">
+                <NavLink to="/" className={e => e.isActive ? "text-blue-500" : 'text-white'}>
+                    Home
+                </NavLink>
             </div>
         </>
     );
